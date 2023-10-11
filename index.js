852946a0d435f9441406175d3a991ab9e1fb7f21
Aillet Gabriel <aillet.gabriel05@gmail.com>
2023-10-11 14:41:43 +0200

    v0.1.3

---
+++ index.js
@@ -0,0 +1,6 @@
+const sofria2WebActions = require("./RenderComponents/Sofria2WebActions")
+const renderers = require('./RenderComponents/render2reactNative')
+module.exports = {
+    sofria2WebActions,
+    renderers
+}
