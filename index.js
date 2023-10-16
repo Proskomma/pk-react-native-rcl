ca7681b6d036520eda20b6c86eb5ae7a6dc9b93a
Aillet Gabriel <aillet.gabriel05@gmail.com>
2023-10-16 11:58:42 +0200

    publish export

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const sofria2WebActions = require("./Sofria2ProskommaRenderFunctions/Sofria2WebActions")
+const renderers = require("./Sofria2ProskommaRenderFunctions/render2reactNative")
 const SideViewVerseByVerse = require("./ReadingComponents/SideViewVerseByVerse")
 const ReadingScreen = require("./ReadingComponents/ReadingScreen")
 module.exports = {
     SideViewVerseByVerse,
+    renderers,
+    sofria2WebActions,
     ReadingScreen
 }
