1c24b3862edcef7691bec1cb11d98f6c4805216b
Aillet Gabriel <aillet.gabriel05@gmail.com>
2023-10-16 11:36:15 +0200

    Add of rendering Components

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sofria2WebActions = require("./Sofria2ProskommaRenderFunctions/Sofria2WebActions")
-const renderers = require('./Sofria2ProskommaRenderFunctions/render2reactNative')
+const SideViewVerseByVerse = require("./ReadingComponents/SideViewVerseByVerse")
+const ReadingScreen = require("./ReadingComponents/ReadingScreen")
 module.exports = {
-    sofria2WebActions,
-    renderers
+    SideViewVerseByVerse,
+    ReadingScreen
 }
